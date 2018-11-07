16fd20ab2543c317e909a9247b0390d69246206a
Uno-B <unumandakh.b@gmail.com>
2018-11-07 22:25:12 +0300

    Updated btn method for changing active player and updating score

--- DOM pig game/app.js
+++ DOM pig game/app.js
@@ -38,7 +38,28 @@ function btn() {
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + ".png";
 
-    // 3. Update
+    // 3. Update the round score if the rolled number was NOT a 1
+    if (dice !== 1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector("#current-" + activePlayer).textContent =
+            roundScore;
+    } else {
+        // Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById("current-0").textContent = 0;
+        document.getElementById("current-1").textContent = 0;
+
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        //document.querySelector(".player-0-panel").classList.remove("active");
+        //document.querySelector(".player-1-panel").classList.add("active");
+
+        document.querySelector(".dice").style.display = "none";
+    }
 }
 
 document.querySelector(".btn-roll").addEventListener("click", btn);
