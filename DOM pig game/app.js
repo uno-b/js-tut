bef93f6290382146f53fd26dc7f767a9cef0b003
Uno-B <unumandakh.b@gmail.com>
2018-11-07 22:05:58 +0300

    Added event listener for the button 'roll dice'

--- DOM pig game/app.js
+++ DOM pig game/app.js
@@ -15,20 +15,39 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).textContent = dice;
 //document.querySelector("#current-" + activePlayer).innerHTML = 
 //   "<em>" + dice + "</em>";
-
-var x = document.querySelector("#score-0").textContent;
-console.log(x);
+//var x = document.querySelector("#score-0").textContent;
+//console.log(x);
 
 document.querySelector(".dice").style.display = "none";
 
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+// getElementById "method" is "faster"
 
+function btn() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
 
+    // 2. Display the result
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
 
+    // 3. Update
+}
+
+document.querySelector(".btn-roll").addEventListener("click", btn);
+
+/************ Anonymous function
+* document.querySelector(".btn-roll").addEventListener("click", function() {
+*    // Do something
+* }); 
+****************************************************************************/
 
 
 
