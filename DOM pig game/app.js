59ddff284bda409197e2adfc6b639fd328a77923
Uno-B <unumandakh.b@gmail.com>
2018-11-08 10:20:42 +0300

    Implemented hold action listener

--- DOM pig game/app.js
+++ DOM pig game/app.js
@@ -16,10 +16,8 @@ roundScore = 0;
 activePlayer = 0;
 
 //document.querySelector("#current-" + activePlayer).textContent = dice;
-//document.querySelector("#current-" + activePlayer).innerHTML = 
-//   "<em>" + dice + "</em>";
+//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
 //var x = document.querySelector("#score-0").textContent;
-//console.log(x);
 
 document.querySelector(".dice").style.display = "none";
 
@@ -46,7 +44,41 @@ function btn() {
             roundScore;
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+    }
+}
+
+document.querySelector(".btn-roll").addEventListener("click", btn);
+
+/************ Anonymous function
+* document.querySelector(".btn-roll").addEventListener("click", function() {
+*    // Do something
+* }); 
+****************************************************************************/
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+
+    // Update the UI
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+
+    // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    } else {
+        // Next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
 
         document.getElementById("current-0").textContent = 0;
@@ -59,20 +91,7 @@ function btn() {
         //document.querySelector(".player-1-panel").classList.add("active");
 
         document.querySelector(".dice").style.display = "none";
-    }
 }
-
-document.querySelector(".btn-roll").addEventListener("click", btn);
-
-/************ Anonymous function
-* document.querySelector(".btn-roll").addEventListener("click", function() {
-*    // Do something
-* }); 
-****************************************************************************/
-
-
-
-
 
 
 
