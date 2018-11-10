675ef26fbe11c7e94ce81450e02a4e5f411e49c9
Uno-B <unumandakh.b@gmail.com>
2018-11-10 22:54:17 +0300

    Added public function for reading input data

--- budgety project/app.js
+++ budgety project/app.js
@@ -8,18 +8,43 @@ var budgetController = (function() {
 
 var UIController = (function() {
     
-    // Some code
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
+
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, 
+                // Will be either inc or exp
+
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMStrings: function() {
+            return DOMStrings;
+        }
+    };
     
 })();
 /*
 * Seperation of concerns - having no connection to other nodes
 */
 
-
+// Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
     
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add the item for the budget controller
 
@@ -28,10 +53,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        
+
     }
     
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOMStrings.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
         if (event.keyCode === 13) {
