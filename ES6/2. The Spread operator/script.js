ebbeceaf40d76741cdba60793dd81456c74d113c
Uno-B <unumandakh.b@gmail.com>
2018-11-16 22:05:55 +0300

    Renamed (Added numerics)

---
+++ ES6/2. The Spread operator/script.js
@@ -0,0 +1,51 @@
+/******************************
+*** The Spread operator (...)
+*** Can be used on iterable such as arrays or strings
+******************************/
+
+function addFourAges(a, b, c, d) {
+	return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+
+const familySmith = ["John", "Jane", "Mark"];
+const familyMiller = ["Mary", "Bob", "Ann"];
+const bigFamily = [...familySmith, "Lily", ...familyMiller];
+
+
+const h = document.querySelector("h1");
+const boxes = document.querySelectorAll(".box");
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur =>
+	cur.style.color = "purple");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
