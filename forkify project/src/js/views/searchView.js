7b09a178347b20d8594782d76529766e7db07a82
Uno-B <unumandakh.b@gmail.com>
2018-11-27 20:42:24 +0200

    Added next prev icons for changing pages to the DOM

--- forkify project/src/js/views/searchView.js
+++ forkify project/src/js/views/searchView.js
@@ -42,8 +42,43 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+// type: "prev" or "next"
+const createButton = (page, type) => `
+                <button class="btn-inline results__btn--${type}" data-goto=${type === "prev" ? page - 1 : page + 1}>
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-triangle-${type === "prev" ? "left" : "right"}></use>
+                </svg>
+                <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+                </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    let button;
+    if (page === 1 && pages > 1) {
+        // only button to go to the next page
+        button = createButton(page, "next");
+    } else if (page < pages) {
+        // both buttons
+        button = `
+            createButton(page, "next")
+            createButton(page, "next")
+        `;
+
+    } else if (page === pages && pages > 1) {
+        // only button to go to the prev page
+        button = createButton(page, "prev");
+    }
+
+    
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
 };
 
 
