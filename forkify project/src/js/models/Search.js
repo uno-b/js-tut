63e7318b6c65e8ffa8c6250e5e3af578f7a6f3bf
Uno-B <unumandakh.b@gmail.com>
2018-11-30 19:55:19 +0200

    Created a Recipe model that receives the individual recipe's data

--- forkify project/src/js/models/Search.js
+++ forkify project/src/js/models/Search.js
@@ -1,13 +1,12 @@
 import axios from "axios";
+import { key } from "../config"
 
 export default class Search {
-    
     constructor(query) {
         this.query = query;
     }
 
     async getResults() {
-        const key = "e97059c34c37f6468c87297527151933";
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
         this.result = res.data.recipes;
